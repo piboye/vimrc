f0799120a7efbc0433d438c0da43dca18f4492d2
piboye <piboye@qq.com>
2013-12-30 00:33:46 +0800

    easy setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-cp .vimrc ~/.vimrc
+mv ~/.vimrc vimrc_bak_$(date +%Y%m%d)
+ln -s .vimrc ~/.vimrc
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+
 npm install -g js-beautify
+
+# install ack
+curl http://beyondgrep.com/ack-2.12-single-file > ~/bin/ack && chmod 0755 !#:3
